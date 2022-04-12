747ff3b3751f601b3c5aae6a2830671b560a931c
Maksim_KA <mad_2004@inbox.ru>
2022-04-12 21:30:31 +0500

    Добавьте файлы проекта.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿
+namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog2 = new System.Windows.Forms.FolderBrowserDialog();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open file bad words";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 111);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Start search";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(161, 111);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(217, 52);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(161, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(217, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 41);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(121, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Open file directory";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(161, 43);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(217, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(13, 71);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(120, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Svae directory";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(161, 73);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(217, 20);
+            this.textBox3.TabIndex = 8;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(399, 14);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(256, 147);
+            this.listBox1.TabIndex = 9;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(13, 140);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(667, 171);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        class test {
+            static public bool wok = false;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    var filePath = openFileDialog1.FileName;
+                    using (Stream str = openFileDialog1.OpenFile())
+                    {
+                        textBox1.Text = filePath.ToString();
+                    }
+                }
+                catch (SecurityException ex)
+                {
+                    MessageBox.Show($"Security error.\n\nError message: {ex.Message}\n\n" +
+                    $"Details:\n\n{ex.StackTrace}");
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Thread thread = new Thread(new ParameterizedThreadStart(readFile));
+
+            if (thread.ThreadState.ToString() == "Unstarted") thread.Start();
+
+            if (thread.ThreadState.ToString() == "Running")
+            {
+                test.wok = true;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox2.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog2.ShowDialog() == DialogResult.OK)
+            {
+                textBox3.Text = folderBrowserDialog2.SelectedPath;
+            }
+        }
+
+        private void readFile(object _obj)
+        {            
+            string[] fileBadWorns = File.ReadAllLines(openFileDialog1.FileName, System.Text.Encoding.GetEncoding(1251));
+            Dictionary<string, int> statBadWords = new Dictionary<string, int>();
+
+            var fileInDic = Directory.EnumerateFiles(folderBrowserDialog1.SelectedPath, "*.txt", SearchOption.AllDirectories);
+            int statBar = (100 / fileInDic.Count()) - (1 * fileInDic.Count() / 100);
+
+            foreach (var findedFile in fileInDic)
+            {
+                while (!test.wok)
+                {
+                    Thread.Sleep(500);
+                }
+
+                FileInfo fileInf = new FileInfo(findedFile);
+                bool flagToReplace = false;
+                Dictionary<string, int> findBadWords = new Dictionary<string, int>();
+
+                string fileText = File.ReadAllText(findedFile.ToString(), System.Text.Encoding.GetEncoding(1251));
+
+                listBox1.Invoke(new Action(() => listBox1.Items.Add(findedFile.ToString() + " \t" + fileInf.Length + " байт")));
+
+                foreach (var carrentWord in fileText.Split(' ', '.', ',', '!', '?', '\n', '\r'))
+                {
+                    foreach (var badWord in fileBadWorns)
+                    {
+                        if (carrentWord == badWord)
+                        {
+                            flagToReplace = true;
+                            if (findBadWords.ContainsKey(badWord))
+                            {
+                                int tempVounter = findBadWords[badWord];
+                                tempVounter += 1;
+                                findBadWords.Remove(badWord);
+                                findBadWords.Add(badWord, tempVounter);
+                            } else
+                            {
+                                findBadWords.Add(badWord, 1);
+                            }
+                        }
+                    }
+                }
+
+                foreach (var item in findBadWords)
+                {
+                    if (statBadWords.ContainsKey(item.Key))
+                    {
+                        int tempVounter = statBadWords[item.Key];
+                        tempVounter += item.Value;
+                        statBadWords.Remove(item.Key);
+                        statBadWords.Add(item.Key, tempVounter);
+                    } else
+                    {
+                        statBadWords.Add(item.Key, item.Value);
+                    }
+                }
+
+                if (flagToReplace)
+                {
+                    foreach (var item in fileBadWorns)
+                    {
+                        fileText = fileText.Replace(item, "*******");
+                    }
+                    File.WriteAllText(folderBrowserDialog2.SelectedPath + "\\" + Path.GetFileName(findedFile), fileText);
+                }
+
+                progressBar1.Invoke(new Action(() => progressBar1.Value += statBar));
+
+                foreach (var item in findBadWords)
+                {
+                    listBox1.Invoke(new Action(()=>listBox1.Items.Add("   " + item.Key + " - " + item.Value)));
+                }
+                Thread.Sleep(1000);
+            }
+
+            listBox1.Invoke(new Action(() => listBox1.Items.Add("")));
+            listBox1.Invoke(new Action(() => listBox1.Items.Add("- - - - - - топ 10 слов - - - - - -")));
+            int counter = 1;
+            foreach (var pair in statBadWords.OrderByDescending(pair => pair.Value))
+            {
+                if (counter > 10)
+                {
+                    break;
+                }
+                listBox1.Invoke(new Action(() => listBox1.Items.Add(counter + ". " + pair.Key + " - " + pair.Value)));
+                counter++;
+            }
+            progressBar1.Invoke(new Action(() => progressBar1.Value = 100));
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            if (test.wok)
+            {
+                test.wok = false;
+            }
+        }
+    }
+}
